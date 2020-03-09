11608a404b27c2a1cf8114f27d5c369933ab8ae2
Dejan Antic <dex.antic@gmail.com>
2020-03-09 20:36:32 +0100

    Revert "Started implementing edit fuction for transactions"
    This reverts commit b27ba97bc5d757dee478c7360428b94d7e350f66.

--- app.js
+++ app.js
@@ -7,29 +7,6 @@ var inputDate = document.querySelector('#date');
 var today = new Date().toISOString().slice(0, 10);
 inputDate.value = today;
 
-document.addEventListener('DOMContentLoaded', function () {
-    for (var i = 0; i < transactions.length; i++) {
-        buildTransactionHTML(transactions[i]);
-    }
-
-    var editTriggers = document.getElementsByClassName('transaction__group');
-    for (var i = 0; i < editTriggers.length; i++) {
-        editTriggers[i].addEventListener('click', function (e) {
-            e.stopPropagation();
-            var transactionID = e.target.closest('li').getAttribute('data-transaction-id');
-            for (var j = 0; j < transactions.length; j++) {
-                if (transactions[j].id === Number(transactionID, 10)) {
-                    activeTransaction = transactions[j];
-                    toggleNewTransactionSection();
-                    document.querySelector('#description').value = activeTransaction.desc;
-                    document.querySelector('#amount').value = parseInt(activeTransaction.amount);
-                    inputDate = document.querySelector('#date').value = activeTransaction.date.split('/').reverse().join('-');
-                    addNewTransaction(activeTransaction);
-                }
-            }
-        });
-    }
-});
 
 // Toggle .js-active on transaction click, remove .js-active if click outside
 // transactionsList
@@ -47,9 +24,8 @@ document.addEventListener('click', function toggleJsActive(e) {
 
     var transactions = document.querySelectorAll('.js-transaction');
     var clickedTransaction = e.target.closest('li');
-    if (clickedTransaction) {
-        clickedTransaction.classList.toggle('js-active');
-    }
+
+    clickedTransaction.classList.toggle('js-active');
 
     // Loop through all transactions and remove '.js-active' except the clicked
     // transaction
@@ -57,10 +33,8 @@ document.addEventListener('click', function toggleJsActive(e) {
 
         var transaction = transactions[i];
 
-        if (clickedTransaction) {
-            // If transaction === clicked transaction, skip to next iteration
-            if (transaction.dataset.transactionId === clickedTransaction.dataset.transactionId) continue;
-        }
+        // If transaction === clicked transaction, skip to next iteration
+        if (transaction.dataset.transactionId === clickedTransaction.dataset.transactionId) continue;
 
         transaction.classList.remove('js-active');
     }
@@ -115,41 +89,36 @@ newTransactionSection.addEventListener('click', function hideNewTransactionSecti
 // Add new transaction
 var form = document.querySelector('.js-form');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', function addNewTransaction(e) {
+
     // By preventing the default action, we have to reset the form if we want
     // to clear the previous user input TK
     e.preventDefault();
-    addNewTransaction(activeTransaction);
-    // Remove the popup after the new transaction is added
-    toggleNewTransactionSection();
 
-});
-
-function addNewTransaction(activeTransaction) {
 
     var inputDescription = document.querySelector('#description').value,
         inputAmount = parseInt(document.querySelector('#amount').value),
         inputDate = document.querySelector('#date').value;
 
-    if (activeTransaction.id === null) {
-        var transaction = new Transaction(inputDescription, inputAmount, inputDate);
 
-        // Push new transaction in transactions array
-        transactions.push(transaction);
-
-        // Push transaction into local storage
-        // TK
+    var transaction = new Transaction(inputDescription, inputAmount, inputDate);
 
 
-        // Build the HTML of new transaction
-        buildTransactionHTML(transaction);
-    } else {
-        activeTransaction.desc = inputDescription;
-        activeTransaction.amount = inputAmount;
-        activeTransaction.date = inputDate;
-        activeTransaction = null;
-    }
-}
+    // Push new transaction in transactions array
+    transactions.push(transaction);
+    console.log(transactions);
+
+
+    // Push transaction into local storage
+    // TK
+
+
+    // Build the HTML of new transaction
+    buildTransactionHTML(transaction);
+
+    // Remove the popup after the new transaction is added
+    toggleNewTransactionSection();
+})
 
 
 // Reset the form and toggle .js-shown from the new transaction section
@@ -175,20 +144,6 @@ document.addEventListener('click', function deleteTransaction(e) {
     // TK remove removed transaction from the transactions array
 })
 
-var editTriggers = document.getElementsByClassName('transaction__group');
-for (var i = 0; i < editTriggers.length; i++) {
-    editTriggers[i].addEventListener('click', function (e) {
-        e.stopPropagation();
-        var transactionID = e.target.closest('li').getAttribute('data-transaction-id');
-        for (var j = 0; j < transactions.length; i++) {
-            console.log(Number(transactionID, 10));
-            if (transactions[i].id === Number(transactionID, 10)) {
-                console.log(transactions[i]);
-            }
-        }
-    });
-}
-
 
 /*****************************************************************************/
 /***********************   FUNCTION DECLARATIONS   ***************************/
@@ -196,22 +151,7 @@ for (var i = 0; i < editTriggers.length; i++) {
 
 
 // Array to store the transactions
-var transactions = [
-    {
-        id: 99,
-        desc: 'Salary',
-        amount: 2500.00,
-        date: '24/02/2020'
-    },
-    {
-        id: 100,
-        desc: 'iPhone',
-        amount: -650.30,
-        date: '15/02/2020'
-    }
-];
-
-var activeTransaction = null;
+var transactions = [];
 
 
 // Create new transaction ID
